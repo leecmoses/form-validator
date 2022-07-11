997f6e72c70b8a80cbee9da1c99ff5ba7fce9432
moses <leecmoses@gmail.com>
2022-07-11 13:35:42 -0400

    Validate passwords and store user data

--- script.js
+++ script.js
@@ -7,23 +7,66 @@ const messageContainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
 
 let isValid = false;
+let passwordsMatch = false;
 
 const validateForm = () => {
     // Using Contraint API
     isValid = form.checkValidity();
 
     // Style main message for an error
-    message.textContent = 'Please fill out all fields.';
-    message.style.color = 'red';
-    messageContainer.style.borderColor = 'red';
+    if (!isValid) {
+        message.textContent = 'Please fill out all fields.';
+        message.style.color = 'red';
+        messageContainer.style.borderColor = 'red';
+        // Abort if invalid
+        return;
+    } 
+    // Check to see if passwords match
+    if (password1El.value === password2El.value) {
+        passwordsMatch = true;
+        password1El.style.borderColor = 'green';
+        password2El.style.borderColor = 'green';
+    } else {
+        passwordsMatch = false;
+        message.textContent = 'Make sure passwords match.';
+        message.style.color = 'red';
+        messageContainer.style.borderColor = 'red';
+        password1El.style.borderColor = 'red';
+        password2El.style.borderColor = 'red';
+        // Abort if invalid
+        return;
+    }
+
+    // Success!
+    if (isValid && passwordsMatch) {
+        message.textContent = 'Successfully Registered!';
+        message.style.color = 'green';
+        messageContainer.style.borderColor = 'green';
+    }
 };
 
+const storeFormData = () => {
+    const user = {
+        name: form.name.value,
+        phone: form.phone.value,
+        email: form.email.value,
+        website: form.website.value,
+        password: form.password.value
+    };
+
+    // Do something with user data
+    console.log(user);
+};
 
 const processFormData = e => {
     e.preventDefault();
 
     // Validate Form
     validateForm();
+    // Submit data if valid
+    if (isValid && passwordsMatch) {
+        storeFormData();
+    }
 };
 
 // Event Listener
